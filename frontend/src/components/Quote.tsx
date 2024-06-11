60fd348b409b258b47cd050bb6cc50e534731f30
bhaveshittadwar <bitsadwar@gmail.com>
2024-06-11 17:30:08 -0400

    feat: init frontend & signup component

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,16 @@
+export const Quote = () => {
+    return <div className="bg-slate-200 h-screen flex flex-col justify-center items-center">
+        
+        <div className="max-w-lg">
+            <div className="text-3xl font-bold">
+                "The Customer Service I received was exceptional. The support team went above and beyond to address my concerns."
+            </div>
+            <div className="max-w-md text-xl font-semibold mt-4">
+                Julies Winfield
+            </div>
+            <div className="max-w-md text-slate-400 text-sm font-light">
+                CEO | Acme Corp
+            </div>
+        </div>
+    </div>
+}
